6fa77280dc3f3c7743a17e770dac39fdea4b6a49
George <sonicgd@gmail.com>
2019-04-15 09:51:32 +0500

    Use batch mode for storage

--- src/BioEngine.BRC.Importer/Program.cs
+++ src/BioEngine.BRC.Importer/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -193,6 +193,7 @@ namespace BioEngine.BRC.Importer
                 return false;
             }
 
+            await _storage.FinishBatchAsync();
             await _dbContext.SaveChangesAsync();
             await PrintStatsAsync();
 
