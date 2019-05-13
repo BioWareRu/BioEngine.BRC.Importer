ff0294e2c0c275753385f36e09aaf88489109c6a
George <sonicgd@gmail.com>
2019-05-13 17:30:22 +0500

    Remove hardcoded env configuration from modules. Remove duplicated config classes.

--- src/BioEngine.BRC.Importer/Program.cs
+++ src/BioEngine.BRC.Importer/Program.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using BioEngine.BRC.Domain;
+using BioEngine.Core.DB;
 using BioEngine.Core.Logging.Loki;
+using BioEngine.Core.Search.ElasticSearch;
+using BioEngine.Core.Seo;
+using BioEngine.Core.Storage;
+using BioEngine.Extra.IPB;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,16 +18,58 @@ namespace BioEngine.BRC.Importer
     static class Program
     {
         [SuppressMessage("ReSharper", "UseAsyncSuffix")]
-        public static async Task Main()
+        public static async Task Main(string[] args)
         {
-            var bioEngine = new Core.BioEngine(new string[0]);
+            var bioEngine = new Core.BioEngine(args);
             var host = bioEngine
                 .ConfigureServices(collection =>
                 {
                     collection.AddScoped<Importer>();
                     collection.AddSingleton<IHostedService, ImporterService>();
                 })
+                .AddModule<PostgresDatabaseModule, PostgresDatabaseModuleConfig>((config, configuration, env) =>
+                {
+                    config.Host = configuration["BE_POSTGRES_HOST"];
+                    config.Port = int.Parse(configuration["BE_POSTGRES_PORT"]);
+                    config.Username = configuration["BE_POSTGRES_USERNAME"];
+                    config.Password = configuration["BE_POSTGRES_PASSWORD"];
+                    config.Database = configuration["BE_POSTGRES_DATABASE"];
+                    config.EnablePooling = env.IsDevelopment();
+                })
+                .AddModule<BrcDomainModule>()
+                .AddModule<S3StorageModule, S3StorageModuleConfig>((config, configuration, env) =>
+                {
+                    var uri = configuration["BE_STORAGE_PUBLIC_URI"];
+                    var success = Uri.TryCreate(uri, UriKind.Absolute, out var publicUri);
+                    if (!success)
+                    {
+                        throw new ArgumentException($"URI {uri} is not proper URI");
+                    }
+
+                    var serverUriStr = configuration["BE_STORAGE_S3_SERVER_URI"];
+                    success = Uri.TryCreate(serverUriStr, UriKind.Absolute, out var serverUri);
+                    if (!success)
+                    {
+                        throw new ArgumentException($"S3 server URI {uri} is not proper URI");
+                    }
+
+                    config.PublicUri = publicUri;
+                    config.ServerUri = serverUri;
+                    config.Bucket = configuration["BE_STORAGE_S3_BUCKET"];
+                    config.AccessKey = configuration["BE_STORAGE_S3_ACCESS_KEY"];
+                    config.SecretKey = configuration["BE_STORAGE_S3_SECRET_KEY"];
+                })
+                .AddModule<ElasticSearchModule, ElasticSearchModuleConfig>(
+                    (config, configuration, env) =>
+                    {
+                        config.Url = configuration["BE_ELASTICSEARCH_URI"];
+                        config.Login = configuration["BE_ELASTICSEARCH_LOGIN"];
+                        config.Password = configuration["BE_ELASTICSEARCH_PASSWORD"];
+                    })
                 .AddModule<LokiLoggingModule, LokiLoggingConfig>()
+                .AddModule<SeoModule>()
+                .AddModule<IPBSiteModule>()
+                .GetHostBuilder().UseEnvironment("Development")
                 .Build();
 
             await host.RunAsync();
