208bb205986735bbb9a73a03923172c3ad38ae23
George <sonicgd@gmail.com>
2019-05-13 20:32:01 +0500

    Force to fill required config values

--- src/BioEngine.BRC.Importer/Program.cs
+++ src/BioEngine.BRC.Importer/Program.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using BioEngine.BRC.Domain;
-using BioEngine.Core.DB;
 using BioEngine.Core.Logging.Loki;
-using BioEngine.Core.Search.ElasticSearch;
 using BioEngine.Core.Seo;
-using BioEngine.Core.Storage;
 using BioEngine.Extra.IPB;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,46 +24,12 @@ namespace BioEngine.BRC.Importer
                     collection.AddScoped<Importer>();
                     collection.AddSingleton<IHostedService, ImporterService>();
                 })
-                .AddModule<PostgresDatabaseModule, PostgresDatabaseModuleConfig>((config, configuration, env) =>
-                {
-                    config.Host = configuration["BE_POSTGRES_HOST"];
-                    config.Port = int.Parse(configuration["BE_POSTGRES_PORT"]);
-                    config.Username = configuration["BE_POSTGRES_USERNAME"];
-                    config.Password = configuration["BE_POSTGRES_PASSWORD"];
-                    config.Database = configuration["BE_POSTGRES_DATABASE"];
-                    config.EnablePooling = env.IsDevelopment();
-                })
+                .AddPostgresDb()
                 .AddModule<BrcDomainModule>()
-                .AddModule<S3StorageModule, S3StorageModuleConfig>((config, configuration, env) =>
-                {
-                    var uri = configuration["BE_STORAGE_PUBLIC_URI"];
-                    var success = Uri.TryCreate(uri, UriKind.Absolute, out var publicUri);
-                    if (!success)
-                    {
-                        throw new ArgumentException($"URI {uri} is not proper URI");
-                    }
-
-                    var serverUriStr = configuration["BE_STORAGE_S3_SERVER_URI"];
-                    success = Uri.TryCreate(serverUriStr, UriKind.Absolute, out var serverUri);
-                    if (!success)
-                    {
-                        throw new ArgumentException($"S3 server URI {uri} is not proper URI");
-                    }
-
-                    config.PublicUri = publicUri;
-                    config.ServerUri = serverUri;
-                    config.Bucket = configuration["BE_STORAGE_S3_BUCKET"];
-                    config.AccessKey = configuration["BE_STORAGE_S3_ACCESS_KEY"];
-                    config.SecretKey = configuration["BE_STORAGE_S3_SECRET_KEY"];
-                })
-                .AddModule<ElasticSearchModule, ElasticSearchModuleConfig>(
-                    (config, configuration, env) =>
-                    {
-                        config.Url = configuration["BE_ELASTICSEARCH_URI"];
-                        config.Login = configuration["BE_ELASTICSEARCH_LOGIN"];
-                        config.Password = configuration["BE_ELASTICSEARCH_PASSWORD"];
-                    })
-                .AddModule<LokiLoggingModule, LokiLoggingConfig>()
+                .AddElasticSearch()
+                .AddS3Storage()
+                .AddModule<LokiLoggingModule, LokiLoggingConfig>((configuration, environment) =>
+                    new LokiLoggingConfig(configuration["BRC_LOKI_URL"]))
                 .AddModule<SeoModule>()
                 .AddModule<IPBSiteModule>()
                 .GetHostBuilder().UseEnvironment("Development")
