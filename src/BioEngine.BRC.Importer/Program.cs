595ca1cc17750e7a01f19600fefb23afc8dabd88
George <sonicgd@gmail.com>
2019-05-15 09:09:24 +0500

    Refactor common lib, move views from site apps to reduce duplications

--- src/BioEngine.BRC.Importer/Program.cs
+++ src/BioEngine.BRC.Importer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using BioEngine.BRC.Common;
 using BioEngine.BRC.Domain;
 using BioEngine.Core.Logging.Loki;
 using BioEngine.Core.Seo;
