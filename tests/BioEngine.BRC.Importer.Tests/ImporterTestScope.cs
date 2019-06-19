9174ccc5e8785df06c1eadb9812906b560ec9f17
George <sonicgd@gmail.com>
2019-06-19 10:59:57 +0500

    Use file storage for test runs

--- tests/BioEngine.BRC.Importer.Tests/ImporterTestScope.cs
+++ tests/BioEngine.BRC.Importer.Tests/ImporterTestScope.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using BioEngine.BRC.Common;
 using BioEngine.Core.Seo;
+using BioEngine.Core.Storage;
 using BioEngine.Core.Tests;
 using BioEngine.Extra.Facebook;
 using BioEngine.Extra.Twitter;
@@ -18,7 +21,12 @@ namespace BioEngine.BRC.Importer.Tests
                     collection.AddScoped<FilesUploader>();
                     collection.AddHttpClient();
                     collection.AddScoped<HtmlParser>();
-                }).AddBrcDomain().AddS3Storage().AddModule<SeoModule>()
+                }).AddBrcDomain().AddModule<FileStorageModule, FileStorageModuleConfig>((configuration, environment) =>
+                {
+                    var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempDirectory);
+                    return new FileStorageModuleConfig(new Uri("https://test.bioware.ru"), tempDirectory);
+                }).AddModule<SeoModule>()
                 .AddModule<TwitterModule>()
                 .AddElasticSearch()
                 .AddModule<FacebookModule>().ConfigureAppConfiguration(builder =>
